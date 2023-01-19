f52f626a75c53b408a5b17aa8eda77966a338c64
maiconmota <maiconlmotadev@hotmail.com>
2023-01-19 13:10:06 +0000

    Update

---
+++ Web_An_Cs/WebApi/Token/JwtSecurityKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApi.Token
+{
+	public class JwtSecurityKey
+	{
+		public static SymmetricSecurityKey Create(string secret)
+		{
+			return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+		}
+	}
+}
+
