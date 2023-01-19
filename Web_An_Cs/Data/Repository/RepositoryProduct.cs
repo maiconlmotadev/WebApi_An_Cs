74d6ad0f3d3a757f8a5143a3d2e95802ea6d314d
maiconmota <maiconlmotadev@hotmail.com>
2023-01-19 12:53:16 +0000

    creation project WebApi

---
+++ Web_An_Cs/Data/Repository/RepositoryProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Data.Entities;
+using Data.Interfaces;
+
+namespace Data.Repository
+{
+	public class RepositoryProduct : RepositoryGenerics<Product>, IProduct
+	{
+		
+	}
+}
+
