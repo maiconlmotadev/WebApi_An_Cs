8ce1b2525bc673094b6bd0aa7d13948f46c3589b
Maicon Mota <88989642+maiconlmotadev@users.noreply.github.com>
2023-01-26 14:39:41 +0000

    Up

---
+++ WebApi/Controllers/ProductApiController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Data.Entities;
+using Data.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    public class ProductApiController : Controller
+    {
+        private readonly IProduct IProduct;
+
+        public ProductApiController(IProduct IProduct)
+        {
+            this.IProduct = IProduct;
+        }
+
+        [HttpGet("/api/ListProducts")]
+        public async Task<JsonResult> ListProducts()
+        {
+            return Json(await this.IProduct.List());
+        }
+
+        [HttpPost("/api/AddProduct")]
+        public async Task AddProduct([FromBody] Product product)
+        {
+            await Task.FromResult(this.IProduct.Add(product));
+        }
+    }
+
+    
+    
+}
+
---
+++ WebApi/Program.cs
@@ -0,0 +1,99 @@
+﻿using Data.Config;
+using Data.Entities;
+using Data.Interfaces;
+using Data.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebApi.Data;
+using WebApi.Token;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlite(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ContextBase>(options =>
+  options.UseSqlServer(
+      builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextBase>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// singletons das interfaces
+
+builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGenerics<>)); 
+builder.Services.AddSingleton<IProduct, RepositoryProduct>();
+
+
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//           .AddJwtBearer(option =>
+//           {
+//               option.TokenValidationParameters = new TokenValidationParameters
+//               {
+//                   ValidateIssuer = false,
+//                   ValidateAudience = false,
+//                   ValidateLifetime = true,
+//                   ValidateIssuerSigningKey = true,
+
+//                   ValidIssuer = "Teste.Securiry.Bearer",
+//                   ValidAudience = "Teste.Securiry.Bearer",
+//                   IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+//               };
+
+//               option.Events = new JwtBearerEvents
+//               {
+//                   OnAuthenticationFailed = context =>
+//                   {
+//                       Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+//                       return Task.CompletedTask;
+//                   },
+//                   OnTokenValidated = context =>
+//                   {
+//                       Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+//                       return Task.CompletedTask;
+//                   }
+//               };
+//           });
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
+
